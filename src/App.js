699a2a5bcf1fef4fffa980511dfb602e510b6732
patr4519 <jero96@mail.ru>
2023-02-06 19:30:52 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-
 import "./scss/app.scss";
-
 import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
@@ -9,16 +7,16 @@ import PizzaBlock from "./components/PizzaBlock";
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <Header />
-      <div class="content">
-        <div class="container">
-          <div class="content__top">
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
             <Categories />
             <Sort />
           </div>
-          <h2 class="content__title">Все пиццы</h2>
-          <div class="content__items">
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
             <PizzaBlock />
             <PizzaBlock />
             <PizzaBlock />
