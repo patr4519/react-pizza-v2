14ee2e5ee1d1d69b1e977073c6bd96bedc25cd3c
patr4519 <jero96@mail.ru>
2023-02-11 12:19:34 +0300

    my own improvments

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -10,20 +10,51 @@ const Search = ({ searchValue, setSearchValue }) => {
         placeholder="Поиск пиццы..."
         value={searchValue}
       />
+      <svg
+        className={styles.search}
+        width="26"
+        height="26"
+        viewBox="0 0 24 24"
+        fill="none"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M14.3764 12.4769C16.0464 10.1337 15.8302 6.85958 13.728 4.75736C11.3848 2.41421 7.58586 2.41421 5.24271 4.75736C2.89957 7.10051 2.89957 10.8995 5.24271 13.2426C7.34494 15.3449 10.619 15.561 12.9622 13.8911L18.6777 19.6066L20.092 18.1924L14.3764 12.4769ZM12.3138 6.17157C13.8759 7.73367 13.8759 10.2663 12.3138 11.8284C10.7517 13.3905 8.21902 13.3905 6.65692 11.8284C5.09483 10.2663 5.09483 7.73367 6.65692 6.17157C8.21902 4.60948 10.7517 4.60948 12.3138 6.17157Z"
+          fill="black"
+        />
+      </svg>
+
       {searchValue && (
-        <svg
-          onClick={() => setSearchValue('')}
-          className={styles.clearIcon}
-          width="800px"
-          height="800px"
-          viewBox="0 0 1024 1024"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill="#000000"
-            d="M764.288 214.592 512 466.88 259.712 214.592a31.936 31.936 0 0 0-45.12 45.12L466.752 512 214.528 764.224a31.936 31.936 0 1 0 45.12 45.184L512 557.184l252.288 252.288a31.936 31.936 0 0 0 45.12-45.12L557.12 512.064l252.288-252.352a31.936 31.936 0 1 0-45.12-45.184z"
-          />
-        </svg>
+        <>
+          <svg
+            onClick={() => setSearchValue("")}
+            className={styles.clearIcon}
+            width="800px"
+            height="800px"
+            viewBox="0 0 1024 1024"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#000000"
+              d="M764.288 214.592 512 466.88 259.712 214.592a31.936 31.936 0 0 0-45.12 45.12L466.752 512 214.528 764.224a31.936 31.936 0 1 0 45.12 45.184L512 557.184l252.288 252.288a31.936 31.936 0 0 0 45.12-45.12L557.12 512.064l252.288-252.352a31.936 31.936 0 1 0-45.12-45.184z"
+            />
+          </svg>
+          <svg
+            className={styles.line}
+            fill="none"
+            height="44"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1"
+            viewBox="0 0 24 24"
+            width="44"
+          >
+            <path d="M0 0h24v24H0z" fill="none" stroke="none" />
+            <path d="M12 5v14" />
+          </svg>
+        </>
       )}
     </div>
   );
