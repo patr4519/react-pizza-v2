db391a86ff4a5af8e7347098def27da86baaa6f6
patr4519 <jero96@mail.ru>
2023-02-18 20:54:57 +0300

    add some styling

--- src/components/CartEmpty.jsx
+++ src/components/CartEmpty.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import emptyCart from "../assets/img/empty-cart.png";
 
 const CartEmpty = () => {
   return (
-    <>
-      <div>Ваша корзина пустая</div>
+    <div className="cartEmpty">
+      <h2>Ваша корзина пустая</h2>
+      <p>Купите хоть что-нибудь:)</p>
+      <img width={400} height={370} src={emptyCart} alt="empty-cart" />
       <Link to="/">
-        <button>На главную</button>
+        <button className="buttonCart">На главную</button>
       </Link>
-    </>
+    </div>
   );
 };
 
