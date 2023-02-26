833e3da3fea986d93149aa4fe081386f7f8e37c4
patr4519 <jero96@mail.ru>
2023-02-26 19:55:34 +0300

    28.50 time, #24

--- src/redux/slices/pizzasSlice.ts
+++ src/redux/slices/pizzasSlice.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { RootState } from "../store";
 
 export const fetchPizzas = createAsyncThunk(
   "pizza/fetchPizzasStatus",
@@ -13,7 +14,22 @@ export const fetchPizzas = createAsyncThunk(
   }
 );
 
-const initialState = {
+
+type Pizza = {
+  id: string,
+  title: string,
+  price: number,
+  imageUrl: string,
+  sizes: number[],
+  types: number[],
+  rating: number,
+}
+interface PizzaSliceState {
+  items: Pizza[],
+  status: 'loading' | 'success' | 'error',
+}
+
+const initialState: PizzaSliceState = {
   items: [],
   status: "loading",
 };
@@ -44,6 +60,6 @@ const pizzaSlice = createSlice({
 
 export const { setItems } = pizzaSlice.actions;
 
-export const selectPizzaData = (state) => state.pizza;
+export const selectPizzaData = (state: RootState) => state.pizza;
 
 export default pizzaSlice.reducer;
